de3192a11dcbcf123b2bcc651a43d7c0b987d0b6
Finik <777finik777@gmail.com>
2021-10-22 09:36:47 +0300

    Добавил Menu сцену, настроил переходы

---
+++ Assets/Scripts/MenuScene.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour
+{
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Level");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
